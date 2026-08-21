831e17b55795c9c98c212fdb3b9cb1049fc10dcd
lopess <feikaua03@gmail.com>
2026-08-21 12:18:29 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -331,10 +331,8 @@ app.post("/alta", (req, res) => {
 // START
 // =========================
 
-app.listen(3000, () => {
-
-    console.log(
-        "Hospital Pro rodando em http://localhost:3000"
-    );
-
+const PORT = process.env.PORT
+             || 3000;
+app.listen(PORT, () => {
+    console.log(`Porta ${PORT}`);
 });
